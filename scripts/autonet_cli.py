82d6da97a9a362b2c74a4d67e67d6ab032210e3e
mlindauer <marius.rks@googlemail.com>
2017-07-11 14:30:57 +0200

    ADD valid sets in cli

--- scripts/autonet_cli.py
+++ scripts/autonet_cli.py
@@ -20,18 +20,31 @@ from mini_autonet.autonet import AutoNet
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("-X", help="X to be read with numpy.loadtxt()")
-parser.add_argument("-Y", help="Y to be read with numpy.loadtxt() (not one-hot encoded)")
-parser.add_argument("-C", type=int, help="number of classes")
+parser.add_argument("-X", required=True, help="X to be read with numpy.loadtxt()")
+parser.add_argument("-Y", required=True, help="Y to be read with numpy.loadtxt() (not one-hot encoded)")
+parser.add_argument("--X_valid", default=None, help="validation X to be read with numpy.loadtxt()")
+parser.add_argument("-Y_valid", default=None, help="validation Y to be read with numpy.loadtxt() (not one-hot encoded)")
+parser.add_argument("-C", required=True, type=int, help="number of classes")
 parser.add_argument("-E", type=int, default=100, help="max epochs")
 parser.add_argument("-R", type=int, default=100, help="runcount limit")
 parser.add_argument("-L", type=int, default=10, help="maximum number of layers")
 
 args_ = parser.parse_args()
 
-X = np.loadtxt(args_.X)
-Y = np.loadtxt(args_.Y)
+X_train = np.loadtxt(args_.X)
+Y_train = np.loadtxt(args_.Y)
+
+if args_.X_valid:
+    X_valid = np.loadtxt(args_.X_valid)
+else:
+    X_valid = X_train
+
+if args_.Y_valid:
+    Y_valid = np.loadtxt(args_.Y_valid)
+else:
+    Y_valid = Y_train
+    
 
 an = AutoNet(max_layers=args_.L, n_classes=args_.C)
-an.fit(X_train=X, y_train=Y, X_valid=X, y_valid=Y, 
+an.fit(X_train=X_train, y_train=Y_train, X_valid=X_valid, y_valid=Y_valid, 
        max_epochs=args_.E, runcount_limit=args_.R)
